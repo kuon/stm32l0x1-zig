dfab7cee2ad214b1f23330d250e821657b4cc5d9
Nicolas Goy <kuon@goyman.com>
2022-08-19 23:00:51 +0200

    Do not expose chip specific mode function

--- src/gpio.zig
+++ src/gpio.zig
@@ -18,49 +18,49 @@ pub fn Pin(comptime spec: []const u8) type {
     const port = @field(regs, "GPIO" ++ port_name);
 
     return struct {
-        fn enable_port_clock() void {
+        pub fn enable_port_clock() void {
             set(regs.RCC.IOPENR, "IOP" ++ port_name ++ "EN", 1);
         }
-        fn input() void {
+        pub fn input() void {
             set(port.MODER, "MODE" ++ suffix, 0b00);
         }
-        fn output() void {
+        pub fn output() void {
             set(port.MODER, "MODE" ++ suffix, 0b01);
         }
-        fn alternate() void {
+        pub fn alternate() void {
             set(port.MODER, "MODE" ++ suffix, 0b10);
         }
-        fn analog() void {
+        pub fn analog() void {
             set(port.MODER, "MODE" ++ suffix, 0b11);
         }
-        fn push_pull() void {
+        pub fn push_pull() void {
             set(port.OTYPER, "OT" ++ suffix, 0);
         }
-        fn open_drain() void {
+        pub fn open_drain() void {
             set(port.OTYPER, "OT" ++ suffix, 1);
         }
-        fn low_speed() void {
+        pub fn low_speed() void {
             set(port.OSPEEDR, "OSPEED" ++ suffix, 0x00);
         }
-        fn medium_speed() void {
+        pub fn medium_speed() void {
             set(port.OSPEEDR, "OSPEED" ++ suffix, 0x01);
         }
-        fn high_speed() void {
+        pub fn high_speed() void {
             set(port.OSPEEDR, "OSPEED" ++ suffix, 0x10);
         }
-        fn very_high_speed() void {
+        pub fn very_high_speed() void {
             set(port.OSPEEDR, "OSPEED" ++ suffix, 0x11);
         }
-        fn floating() void {
+        pub fn floating() void {
             set(port.PUPDR, "PUPD" ++ suffix, 0b00);
         }
-        fn pull_up() void {
+        pub fn pull_up() void {
             set(port.PUPDR, "PUPD" ++ suffix, 0b01);
         }
-        fn pull_down() void {
+        pub fn pull_down() void {
             set(port.PUPDR, "PUPD" ++ suffix, 0b10);
         }
-        fn alternate_mode(mode: u3) void {
+        pub fn alternate_mode(mode: u3) void {
             if (pin_number > 7) {
                 set(port.AFRH, "AFSEL" ++ suffix, mode);
             } else {
@@ -72,35 +72,6 @@ pub fn Pin(comptime spec: []const u8) type {
             push_pull();
             output();
             floating();
-        }
-        pub fn spi_mode() void {
-            enable_port_clock();
-            push_pull();
-            pull_up();
-            alternate();
-            // TODO check microzig.chip.name  and pint name for alternate mode
-            alternate_mode(0);
-            medium_speed();
-        }
-        pub fn i2c_mode() void {
-            enable_port_clock();
-            open_drain();
-            floating();
-            alternate();
-            // TODO check microzig.chip.name  and pint name for alternate mode
-            alternate_mode(1);
-        }
-        pub fn lpuart_mode(dir: enum { rx, tx }) void {
-            enable_port_clock();
-            push_pull();
-            if (dir == .rx) {
-                pull_up();
-            } else {
-                floating();
-            }
-            alternate();
-            // TODO check microzig.chip.name  and pint name for alternate mode
-            alternate_mode(6);
         }
         pub fn low() void {
             // Set pin as low
