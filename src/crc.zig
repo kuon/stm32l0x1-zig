8e2f120188baa11465bdf89b33d109e71880a63d
Nicolas Goy <kuon@goyman.com>
2022-09-06 00:29:55 +0200

    Remove unused var fence

--- src/crc.zig
+++ src/crc.zig
@@ -1,10 +1,9 @@
 const microzig = @import("microzig");
 const regs = microzig.chip.registers;
 
-// this produces the same result as 
+// this produces the same result as
 // std.hash.crc.Crc32.hash(&buf),
 pub fn crc(buf: []const u8) u32 {
-    _ = buf;
     regs.CRC.INIT.modify(.{ .CRC_INIT = 0xffffffff });
     regs.CRC.POL.modify(.{
         .Polynomialcoefficients = 0x4C11DB7,
